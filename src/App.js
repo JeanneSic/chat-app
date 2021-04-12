48ed1f0b11871caf0e8b7bdaf694d24a4f61ab58
JeanneSic <siche.jeanne@gmail.com>
2021-04-12 20:01:08 +0200

    adding list of components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
@@ -9,15 +10,7 @@ function App() {
     avatar='https://randomuser.me/api/portraits/women/33.jpg'
     online/>
 
-<Contact 
-    name='Jenny Day'
-    avatar='https://randomuser.me/api/portraits/women/72.jpg'
-    />
-
-<Contact 
-    name='Kevin West'
-    avatar='https://randomuser.me/api/portraits/men/36.jpg'
-    online/>
+    <ContactList />
     </div>
   );
 }
